abcf05862feeba4ac086ed6d7846a44555b155d3
Sahory31 <bsahory_sandoval@hotmail.com>
2019-10-31 15:45:48 -0600

    Decode function was added, only uppercase

--- src/cipher.js
+++ src/cipher.js
@@ -6,16 +6,16 @@ encode (offset, string){
   let charPosition = 0;
   let encodeSentence = "";
 
+
   for (let i = 0; i < string.length; i++){
 
-    charPosition = string.charCodeAt(i);
-    newPlace = (parseInt(charPosition) - 65 + parseInt(offset)) % 26 + 65;
-    encodeSentence = encodeSentence + String.fromCharCode(newPlace);
-    }
-
-    return encodeSentence;
-  },
-
+      charPosition = string.charCodeAt(i);
+      newPlace = (parseInt(charPosition) - 65 + parseInt(offset)) % 26 + 65;
+      encodeSentence = encodeSentence + String.fromCharCode(newPlace);
+  }
+        return encodeSentence;
+},
+  
   decode(offset, string){
 
     let newPlace = 0;
@@ -27,8 +27,7 @@ encode (offset, string){
       charPosition = string.charCodeAt(i);
       newPlace = (parseInt(charPosition) + 65 - parseInt(offset)) % 26 + 65;
       encodeSentence = encodeSentence + String.fromCharCode(newPlace);
-      }
-
-      return encodeSentence;
+    }
+          return encodeSentence;
     }
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 
 
+
+
 function encodeSentence(){
 
   let offset = document.getElementById('offsetEncodeNumber').value;
   let string = document.getElementById('stringMessage').value;
-
+  let uppercase = string.toUpperCase();
   let encodeSentence = window.cipher.encode(offset, string);
 
   document.getElementById('encodeWords').innerText = encodeSentence;
